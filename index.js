a5f423bf8b497c5269c4f53a6e87ba0d361c3900
Amartya Sen <sen@oakland.edu>
2021-11-07 23:17:27 -0500

    adding modules

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // Test command 
-console.log('Hello from NODE.js'); // run from terminal using the command - node index.js
+// console.log('Hello from NODE.js'); // run from terminal using the command - node index.js  OR node index
+
+/**
+ * Note you are NOT going to write the bulk of your code here. 
+ * You codes which will pieces of various functionalities referred to as modules will be coded in different files, and 
+ * you will include them in this file
+ */
+
+/**
+ * Example Exercise -- Create a person module called person.js and include in index.js
+ * 
+ */
+// OLD SYNTAX
+const person = require('./person'); // since this is a file and not a 3rd party installed module
+
+// ES6 syntax
+// import person from './person';
+
+console.log(person);
+//console.log(person.name);
+
+/**
+ * Mini self exercise -2: Create a student module with a student class using which you can add new student record 
+ * Include the module in the index.js file and instantiate a new object of the class
+ */
---
+++ person.js
@@ -0,0 +1,15 @@
+/**
+ * create a person object. Generally you will be using versatile stuff like Classes or Functions etc.
+ */
+
+const person = {
+    name: 'Mary Peterson',
+    age: 32,
+    address: '115 Library Drive'
+}
+
+/**
+ * To use this module in a different file write the following: 
+ */
+// OLD SYNTAX
+module.exports = person;
